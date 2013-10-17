4c05a44f02c1d18eff6910243aa8227c492e4f04
sxu0507 <fuyu365@163.com>
2013-10-17 14:16:15 +0800

    v1

--- src/sxu/ui/ClientApp.java
+++ src/sxu/ui/ClientApp.java
@@ -1,21 +1,37 @@
 package sxu.ui;
 
+import java.awt.AWTException;
+import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.EventQueue;
 import java.awt.GradientPaint;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
+import java.awt.Image;
+import java.awt.MenuItem;
 import java.awt.Paint;
+import java.awt.PopupMenu;
 import java.awt.Shape;
+import java.awt.SystemTray;
+import java.awt.Toolkit;
+import java.awt.TrayIcon;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.geom.RoundRectangle2D;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
 import sxu.tools.CommonTools;
 
-import java.awt.BorderLayout;
-import java.awt.geom.RoundRectangle2D;
-
+/**
+ * 
+ * @ClassName: ClientApp
+ * @author SXU
+ * @date 2013年10月17日 下午1:29:56
+ * @Description: 
+ *		客户端 启动窗口
+ */
 public class ClientApp {
 
 	private JFrame frame;
@@ -55,7 +71,9 @@ public class ClientApp {
 		frame.setUndecorated(true);
 		CommonTools.setWindowCenter(frame);
 		frame.getContentPane().setLayout(new BorderLayout(0, 0));
-		
+		frame.setBackground(new Color(0, 0, 0));
+		this.setTray();
+		// AWTUtilities.setWindowOpacity(frame, 0.9f);  // 设置透明
 		initContentPanel();
 	}
 	
@@ -69,11 +87,9 @@ public class ClientApp {
 	 * @return void
 	 */
 	private void initContentPanel() {
-		final int R = 66;  
-	    final int G = 194;  
-	    final int B = 110;
-		final Paint p = new GradientPaint(0.0f, 0.0f, new Color(R, G, B, 100),  
-	            frame.getWidth(), frame.getHeight(), new Color(R, G, B, 200), true);  
+		
+		final Paint p = new GradientPaint(0.0f, 0.0f, new Color(66, 194, 110, 100),  
+	            frame.getWidth(), frame.getHeight(), new Color(66, 194, 110, 200), true);  
 		panel = new JPanel() {
 			/**
 			 * 
@@ -96,7 +112,60 @@ public class ClientApp {
             }  
 		};
 		frame.setContentPane(panel);
-		
+		panel.setLayout(null);
 	}
-
+	
+	/**
+	 *  
+	 * @Method: setTray
+	 * @author SXU
+	 * @date 2013年10月17日 下午1:28:35
+	 * @Description: 
+	 *		设置托盘小图标和菜单
+	 * @return void
+	 */
+    public void setTray() {  
+          
+        if(SystemTray.isSupported()){//判断当前平台是否支持托盘功能  
+            //创建托盘实例  
+            SystemTray tray = SystemTray.getSystemTray();  
+            //创建托盘图标：1.显示图标Image 2.停留提示text 3.弹出菜单popupMenu 4.创建托盘图标实例  
+            //1.创建Image图像  
+            Image image = Toolkit.getDefaultToolkit().getImage("images/trayICO.jpg");  
+            //2.停留提示text  
+            String text = "MTC";  
+            //3.弹出菜单popupMenu  
+            PopupMenu popMenu = new PopupMenu();  
+            MenuItem itmOpen = new MenuItem("打开");  
+            itmOpen.addActionListener(new ActionListener(){  
+                public void actionPerformed(ActionEvent e) {  
+                    //Show();  
+                }                 
+            });  
+            MenuItem itmHide = new MenuItem("隐藏");  
+            itmHide.addActionListener(new ActionListener(){  
+                public void actionPerformed(ActionEvent e) {  
+                   // UnVisible();  
+                }  
+            });  
+            MenuItem itmExit = new MenuItem("退出");  
+            itmExit.addActionListener(new ActionListener(){  
+                public void actionPerformed(ActionEvent e) {  
+                    //Exit();  
+                }  
+            });  
+            popMenu.add(itmOpen);  
+            popMenu.add(itmHide);  
+            popMenu.add(itmExit);  
+              
+            //创建托盘图标  
+            TrayIcon trayIcon = new TrayIcon(image, text, popMenu);
+            //将托盘图标加到托盘上  
+            try {  
+                tray.add(trayIcon);  
+            } catch (AWTException e1) {  
+                e1.printStackTrace();  
+            }  
+        }  
+    }  
 }
